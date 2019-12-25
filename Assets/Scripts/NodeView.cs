b11977d51d3ab70bc4cfffaa905203b16428e654
Artem Bondar <artem.bondar.dev@gmail.com>
2019-12-25 18:54:01 +0300

    Create NodeView class

---
+++ Assets/Scripts/NodeView.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class NodeView : MonoBehaviour
+{
+    public GameObject tile;
+
+    [Range(0, 0.5f)]
+    public float borderSize = 0.15f;
+
+    private void ColorNode(Color color, GameObject go)
+    {
+        if (go != null)
+        {
+            Renderer goRenderer = go.GetComponent<Renderer>();
+
+            if (goRenderer != null)
+            {
+                goRenderer.material.color = color;
+            }
+        }
+    }
+
+    public void Init(Node node)
+    {
+        if (tile != null)
+        {
+            gameObject.name = $"Node ({node.xIndex}, {node.yIndex})";
+            gameObject.transform.position = node.position;
+
+            tile.transform.localScale = new Vector3(1f - borderSize, 1f, 1f - borderSize);
+        }
+    }
+
+    public void ColorNode(Color color) => ColorNode(color, tile);
+}
