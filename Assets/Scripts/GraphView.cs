c80e259bf6482678f09be793ca731b97dee44343
Artem Bondar <artem.bondar.dev@gmail.com>
2019-12-26 19:56:48 +0300

    Create GraphView and DemoController

---
+++ Assets/Scripts/GraphView.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Graph))]
+public class GraphView : MonoBehaviour
+{
+    public GameObject nodeViewPrefab;
+
+    public Color baseColor = Color.white;
+    public Color wallColor = Color.black;
+
+    public void Init(Graph graph)
+    {
+        if (graph == null)
+        {
+            Debug.Log("GraphView: No graph to initialize!");
+            return;
+        }
+
+        foreach (Node node in graph.nodes)
+        {
+            GameObject instance = Instantiate(nodeViewPrefab, Vector3.zero, Quaternion.identity);
+            
+            NodeView nodeView = instance.GetComponent<NodeView>();
+
+            if (nodeView != null)
+            {
+                nodeView.Init(node);
+
+                if (node.nodeType == NodeType.Blocked)
+                {
+                    nodeView.ColorNode(wallColor);
+                }
+                else
+                {
+                    nodeView.ColorNode(baseColor);
+                }
+            }
+        }
+    }
+}
