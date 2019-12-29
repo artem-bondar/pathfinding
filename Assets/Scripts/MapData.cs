1859a1294be7937e47a3ff06cc0d95ef6dd37a57
Artem Bondar <artem.bondar.dev@gmail.com>
2019-12-29 11:18:40 +0300

    Provide loading map data from file

--- Assets/Scripts/MapData.cs
+++ Assets/Scripts/MapData.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+
+using UnityEngine;
 
 public class MapData : MonoBehaviour
 {
     public int width = 10;
     public int height = 5;
+
+    public TextAsset textAsset;
 
     public int[,] MakeMap()
     {
@@ -16,23 +20,29 @@ public class MapData : MonoBehaviour
                 map[x, y] = 0;
             }
         }
-
-        map[1, 0] = 1;
-        map[1, 1] = 1;
-        map[1, 2] = 1;
-        map[3, 2] = 1;
-        map[3, 3] = 1;
-        map[3, 4] = 1;
-        map[4, 2] = 1;
-        map[5, 1] = 1;
-        map[5, 2] = 1;
-        map[6, 2] = 1;
-        map[6, 3] = 1;
-        map[8, 0] = 1;
-        map[8, 1] = 1;
-        map[8, 2] = 1;
-        map[8, 4] = 1;
     
         return map;
     }
+
+    public List<string> GetTextFromFile(TextAsset textAsset)
+    {
+        List<string> lines = new List<string>();
+
+        if (textAsset != null)
+        {
+            string textData = textAsset.text;
+            string[] delimiters = { "\r\n", "\n"};
+
+            lines.AddRange(textData.Split(delimiters, System.StringSplitOptions.None));
+            lines.Reverse();
+        }
+        else
+        {
+            Debug.LogWarning("MapData: GetTextFromFile Error: invalid TextAsset!");
+        }
+
+        return lines;
+    }
+
+    public List<string> GetTextFromFile() => GetTextFromFile(textAsset);
 }
