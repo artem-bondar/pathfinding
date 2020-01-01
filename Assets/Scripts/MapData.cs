4833b0348b7aef961d35e4fb6c7f38f2b61b31d6
Artem Bondar <artem.bondar.dev@gmail.com>
2020-01-01 11:27:45 +0300

    Provide loading from pixel map

--- Assets/Scripts/MapData.cs
+++ Assets/Scripts/MapData.cs
@@ -10,8 +10,24 @@ public class MapData : MonoBehaviour
     public int height = 0;
 
     public TextAsset textAsset;
+    public Texture2D textureMap;
 
     public string resourcePath = "Mapdata";
+
+    private void Start()
+    {
+        string levelName = SceneManager.GetActiveScene().name;
+
+        if (textureMap == null)
+        {
+            textureMap = Resources.Load<Texture2D>(resourcePath + "/" + levelName);
+        }
+
+        if (textAsset == null)
+        {
+            textAsset = Resources.Load<TextAsset>(resourcePath + "/" + levelName);
+        }
+    }
 
     public void SetDimensions(List<string> textLines)
     {
@@ -29,6 +45,16 @@ public class MapData : MonoBehaviour
     public int[,] MakeMap()
     {
         List<string> lines = new List<string>();
+
+        if (textureMap != null)
+        {
+            lines = GetMapFromTexture(textureMap);
+        }
+        else
+        {
+            lines = GetMapFromTextFile(textAsset);
+        }
+
         lines = GetTextFromFile();
         SetDimensions(lines);
 
@@ -48,7 +74,40 @@ public class MapData : MonoBehaviour
         return map;
     }
 
-    public List<string> GetTextFromFile(TextAsset textAsset)
+    public List<string> GetMapFromTexture(Texture2D texture)
+    {
+        List<string> lines = new List<string>();
+
+        if (texture != null)
+        {
+            for (int y = 0; y < texture.height; y++)
+            {
+                string newLine = "";
+
+                for (int x = 0; x < texture.width; x++)
+                {
+                    if (texture.GetPixel(x, y) == Color.black)
+                    {
+                        newLine += "1";
+                    }
+                    else if (texture.GetPixel(x, y) == Color.white)
+                    {
+                        newLine += "0";
+                    }
+                    else
+                    {
+                        newLine += " ";
+                    }
+                }
+
+                lines.Add(newLine);
+            }
+        }
+
+        return lines;
+    }
+
+    public List<string> GetMapFromTextFile(TextAsset textAsset)
     {
         List<string> lines = new List<string>();
 
@@ -60,22 +119,9 @@ public class MapData : MonoBehaviour
             lines.AddRange(textData.Split(delimiters, System.StringSplitOptions.None));
             lines.Reverse();
         }
-        else
-        {
-            Debug.LogWarning("MapData: GetTextFromFile Error: invalid TextAsset!");
-        }
 
         return lines;
     }
 
-    public List<string> GetTextFromFile()
-    {
-        if (textAsset == null)
-        {
-            string levelName = SceneManager.GetActiveScene().name;
-            textAsset = Resources.Load<TextAsset>(resourcePath + "/" + levelName);
-        }
-
-        return GetTextFromFile(textAsset);
-    }
+    public List<string> GetTextFromFile() => GetMapFromTextFile(textAsset);
 }
